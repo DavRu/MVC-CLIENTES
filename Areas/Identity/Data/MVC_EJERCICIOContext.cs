7f306d473a0a1e161e040773171ff04bd5e6f6a6
pc <pc@DESKTOP-RO1SKCS>
2021-02-25 17:58:09 +0100

    Agregar archivos de proyecto.

---
+++ Areas/Identity/Data/MVC_EJERCICIOContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_EJERCICIO.Data
+{
+    public class MVC_EJERCICIOContext : IdentityDbContext<IdentityUser>
+    {
+        public MVC_EJERCICIOContext(DbContextOptions<MVC_EJERCICIOContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
